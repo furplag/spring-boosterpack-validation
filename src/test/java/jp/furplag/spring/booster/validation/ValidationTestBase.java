c87846e55e6f629315451c263a826334a35f9724
furplag <furplag@users.noreply.github.com>
2017-02-22 11:47:23 +0900

    Create ValidationTestBase.java

---
+++ src/test/java/jp/furplag/spring/booster/validation/ValidationTestBase.java
@@ -0,0 +1,37 @@
+package jp.furplag.spring.booster.validation;
+
+import java.util.Set;
+
+import javax.validation.ConstraintViolation;
+import javax.validation.Validation;
+import javax.validation.Validator;
+import javax.validation.ValidatorFactory;
+
+public abstract class ValidationTestBase {
+
+  protected Validator validator;
+
+  @Before
+  public void before() throws Exception {
+    ValidatorFactory validatorFactory = Validation.buildDefaultValidatorFactory();
+    validator = validatorFactory.getValidator();
+  }
+
+  protected <T> void printViolations(Set<ConstraintViolation<T>> violations) {
+    for (ConstraintViolation<T> violation : violations) {
+      StringBuilder sb = new StringBuilder();
+      sb.append("[Path]" + violation.getPropertyPath() + " ");
+      sb.append("[Value]" + violation.getInvalidValue() + " ");
+      sb.append("[Message]" + violation.getMessage());
+      System.err.println(sb.toString());
+    }
+  }
+
+  protected <T> void validate(Object object, Class<?>... groups) {
+    printViolations(validator.validate(object, groups));
+  }
+
+  protected <T> void validateThis(Class<?>... groups) {
+    validate(this, groups);
+  }
+}
